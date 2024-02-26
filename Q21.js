df6db06784c4ef41e56cbf3475bb2ce00b01181e
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-27 00:52:49 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,3 @@
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var person = { name: "Aliza", fname: "Moin", age: 17 };
+console.log(person);
---
+++ Q21.ts
@@ -0,0 +1,5 @@
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person:{name:string,  fname:string, age:number} = {name: "Aliza", fname: "Moin", age: 17}
+
+console.log(person)
